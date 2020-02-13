8eb9c2637600725ca50c460771045ac50651f5ea
ethanonweller <47670910+ethanonweller@users.noreply.github.com>
2020-02-13 11:06:07 -0500

    Add files via upload
    Grabs host name, host public/private ip and uses smtp to send results in an email

---
+++ host_info.py
@@ -0,0 +1,24 @@
+import smtplib, ssl, socket, urllib.request
+
+port = 465 # for ssl
+smtp_server = "smtp.gmail.com"
+sender_email = "test.python.0530@gmail.com"
+receiver_email = "test.python.0530@gmail.com"
+password = ("AFXiJwe86hY6DhJ")
+
+host_name = socket.gethostname()
+priv_ip = socket.gethostbyname(host_name)
+# to get public address w/ standard library you can request through browser
+pub_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+
+message = """\
+Subject: info
+
+This message is sent from host_info.py. \n""" + "Host Name: ", \
+host_name + " Private IP: \n" + priv_ip + "\n Public IP: " + pub_ip
+
+# Creates a secure SSL context
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
